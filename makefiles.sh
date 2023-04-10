55d369cba12ecc20a3d3da5c626456dc493bb4ca
manentia9328 <manentia9328@connect.durhamtech.edu>
2023-04-10 19:24:37 -0400

    add folder array

--- makefiles.sh
+++ makefiles.sh
@@ -1,20 +1,32 @@
+	# Set our variables at the top
+	file1=/data/public/stuff1/some_file1
+        file2=/data/public/stuff2/some_file1
+	paths=("sales" "marketing" "operations" "finance" "human_resources")
 
-	FILE1=/data/public/stuff1/some_file1
-        FILE2=/data/public/stuff2/some_file1
-
+	# Prompt user for how many files they would like made
 	echo How many files would you like to create?
 	read N
 
-	if test -f "$FILE1" && test -f "$FILE2"; then
+	# Test to see if files found in stuff directories. If they are delete the directories
+	if test -f "$file1" && test -f "$file2"; then
 	  sudo rm -rf /data/public/stuff1
 	  sudo rm -rf /data/public/stuff2
 	fi
 
         sudo mkdir -p /data/public/stuff1
 	sudo mkdir -p /data/public/stuff2
-
+	
+	# Loop through and create N specified number of files in both created directories
         for i in $(seq 1 $N)
         do
                sudo dd if=/dev/zero of=/data/public/stuff1/some_file$i bs=10MB count=1
 	       sudo dd if=/dev/zero of=/data/public/stuff2/some_file$i bs=10MB count=1
         done
+
+	# Loop through and create directories based off of entries in paths array
+	for path in "${paths[@]}"
+	do
+		sudo mkdir -p /data/groups/$path
+	done
+
+	
