f75cbd44c6ef94127d6d1f8a361b4d06b3f6abd1
manentia9328 <124215677+manentia9328@users.noreply.github.com>
2023-04-12 23:19:33 -0400

    Update makefiles.sh
    add helper boys. Fix errors.

--- makefiles.sh
+++ makefiles.sh
@@ -1,54 +1,103 @@
+	# Anthony Manenti - NOS125-150 SP23
+	# Shell script for file and folder creation.
+
 	# Set our variables at the top
 	file1=/data/public/stuff1/some_file1
         file2=/data/public/stuff2/some_file1
 	paths=("sales" "marketing" "operations" "finance" "human_resources")
+	optional_folders=()
 
 	# HELPERS
-	make_folders () { 
+
+	# Boolean and while loop keeps calling nested create function. Allows user to add optional folders until the are finished.
+        create_additional_folders() {
+          should_continue=true
+
+          create() {
+            for folder in "${optional_folders[@]}"
+            do
+              sudo mkdir -p /data/groups/$folder
+            done
+              echo Okay! Our work here is done.
+              should_continue=false
+          }
+
+	# Push to optional folders array unless user inputs 'done'  
+          while $should_continue; do
+            read -p "enter a folder name to add the folder, or type done to complete this process " input
+            case $input in 
+                done ) clear; create;;
+                   * ) clear; optional_folders+=("$input")
+            esac
+          done
+       }
+
+	# Flow control basically, call looping function to keep adding if yes, bounce em out if no.
+	prompt_for_additional_folders() {
+	 read -p "Default folders created! Would you like to create additional folders? (y/n) " yn
+	  case $yn in 
+	    [yY] ) create_additional_folders;;
+	    [nN] ) echo Great! our work here is finished!;;
+	    * ) echo invalid response;;
+          esac
+	}
+	
+	# Creates default folders if user indicates they want them, and also starts stack for adding custom folders.
+	make_folders() { 
          for path in "${paths[@]}"
            do
              sudo mkdir -p /data/groups/$path
            done
-        }		
+           prompt_for_additional_folders
+        }
 
+	# Gotta know if the user is actually inputting integers when we need em.
+	validate_num() {
+	  isnum='^[0-9]+$'
+	    if ! [[ $1 =~ $isnum ]] ; then
+   	      echo "error: Not a number" >&2; exit 1
+	    fi
+	  clear	
+	}		
+
+	# MAIN LOGIC FLOW. 
+	# We use the flow to call helper functions when we need em, easier to deal with than nesting deeply.
+
+	clear
 	# Prompt user for how many files they would like made
-	echo How many files would you like to create? [must be an integer]
+	echo "How many files would you like to create? [must be an integer]. "
 	read N
-
-	isnum='^[0-9]+$'
-
-	if ! [[ $N =~ $isnum ]] ; then
-   	  echo "error: Not a number" >&2; exit 1
-	fi	
+	validate_num $N
+	
+	# Prompt user for the size of filed in MB we are creating
+	echo "How many MBs would you like the files to be? [must be an integer]. "
+	read S
+	validate_num $S
 
 	# Test to see if files found in stuff directories. If they are delete the directories
-	if test -f "$file1" && test -f "$file2"; then
-	  read -p "Files are already present in the target directories. If you continue existing files will be deleted. Are you sure you wish to continue? (y/n) " yn
+	if test -f "$file1" || test -f "$file2"; then
+	  read -p "Files are already present in the target directories. If you continue existing files will be deleted. Are you sure you wish to continue? (y/n). " yn
 	  case $yn in 
-	    [yY] ) sudo rm -rf /data/public/stuff1; sudo rm -rf /data/public/stuff2
-	    break;;
-	    [nN] ) echo aborting script;
-	    exit;;
+	    [yY] ) sudo rm -rf /data/public/stuff1; sudo rm -rf /data/public/stuff2;;
+	    [nN] ) echo aborting script;;
 	    * ) echo invalid response;;
           esac
 	fi
 
-        sudo mkdir -p /data/public/stuff1
-	sudo mkdir -p /data/public/stuff2
+	sudo mkdir -p /data/public/stuff{1,2}
 	
 	# Loop through and create N specified number of files in both created directories
-        for i in $(seq 1 $N)
-        do
-               sudo dd if=/dev/zero of=/data/public/stuff1/some_file$i bs=10MB count=1
-	       sudo dd if=/dev/zero of=/data/public/stuff2/some_file$i bs=10MB count=1
+        for i in $(seq 1 $N) do
+          sudo dd if=/dev/zero of=/data/public/stuff1/some_file$i bs=$S count=1
+	  sudo dd if=/dev/zero of=/data/public/stuff2/some_file$i bs=$S count=1
         done
-
-
-	read -p "Would you like this script to create additional directories for you in /data/groups ?" yn
+        clear
+        
+	# Ask user if they want the default folders.
+	read -p "Would you like to create a set of default folders in data/groups? You will also be given the chance to create custom folders. (y/n) " yn
           case $yn in
-            [yY] ) make_folders
-            [nN] ) echo aborting script;
-            exit;;
+            [yY] ) make_folders;;
+            [nN] ) echo Okay! Our work here is done.;;
             * ) echo invalid response;;
           esac
 	
