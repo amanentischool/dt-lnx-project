8ee4a3515e0bb204fc51e1dceac72db25c1e5528
manentia9328 <manentia9328@connect.durhamtech.edu>
2023-05-01 20:25:30 -0400

    clean code. color format. rename file and delete files which were merged into toolbox.sh

--- toolbox.sh
+++ toolbox.sh
@@ -2,7 +2,7 @@
 # Anthony Manenti - NOS125-150 SP23
 # Interactive shell script combining all scripting and config tasks for NOS125.
 
-# This script was put together with a lot of head scratching, trial and error, and googling. THANK YOU Linux forums and StackOverflow ... and Github!
+# This script was put together with a lot of head scratching, trial and error, and googling. THANK YOU Linux forums, StackOverflow, Github, classmates and faculty!
 # Please feel free to use this script as a reference, to build upon it, and improve it.
 
 # TODOS:
@@ -13,12 +13,28 @@
 # 5) IP address validation. (WIP)
 # 6) Make an array of Linux / Dad jokes and add a random joke picker function to the main menu where a random joke is picked and display.
 
+# GREMLINS:
+# Occasionally auto disk configuration can be a little screwy. (but it does work) Removing files from /data first usually allows the process to go through. I'm not sure what's happening here yet.
+
 # COLOR HELPERS
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No color
+
+# VALIDATION HELPERS
+is_num='^[0-9]+$'
+is_ranged_num='^[0-9]$|^10$'
+
+# these validators are somewhat flawed. They don't take into account valid IP ranges. But atleast it's something.
+# IP Validation Regex (https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp)
+is_cidr='^([0-9]{1,3}\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$'
+is_ip='^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
+
+# OTHER HELPERS
+version="1.0.0"
+heading="Tux ToolBox v $version"
 
 # MARK: CALLER
 
@@ -27,6 +43,8 @@ NC='\033[0m' # No color
 # We don't have to worry about massive entangled blocks. It's a caller basically.
 call_menu() {
     clear
+    echo -e "${YELLOW}$heading${NC}"
+
     echo -e "\n1) Set Hostname\n2) Create Files and Folders\n3) Delete Files and Folders\n4) Configure Networking\n5) Install Git\n6) Configure Storage\n\nQ) Quit\n"
     read -p "Welcome! What would you like to do? " input
     case $input in
@@ -65,9 +83,10 @@ call_menu() {
 set_hostname() {
     clear
     local inval_guard=true
+    echo -e "\n${YELLOW}$heading : HOSTNAME CONFIG${NC}\n"
     read -p "Please enter your hostname : " hostname
     while $inval_guard; do
-        read -p "Your new hostname will be : $hostname. Please confirm this change. (y/n) (q: quit without saving) " ynq
+        read -p "Your new hostname will be : $hostname Please confirm this change. (y/n) (q: quit without saving) " ynq
         case $ynq in
         [yY])
             inval_guard=false
@@ -95,12 +114,12 @@ set_hostname() {
 # Kicks off file and default folder creation chain, sends user input to validate.
 prompt_num_files() {
     clear
+    echo -e "\n${YELLOW}$heading : FILES AND FOLDERS${NC}\n"
     echo "How many files would you like to create? [must be an integer]. "
     read N
-
-    isnum='^[0-9]+$'
-    if ! [[ $N =~ $isnum ]]; then
+    if ! [[ $N =~ $is_num ]]; then
         echo error: Not a number. Try again.
+        sleep 2
         prompt_num_files
     else
         prompt_file_size
@@ -110,11 +129,10 @@ prompt_num_files() {
 # Ask for file size in MB, send to validate.
 prompt_file_size() {
     clear
+    echo -e "\n${YELLOW}$heading : FILES AND FOLDERS${NC}\n"
     echo "How many MBs would you like the files to be? [must be an integer]. "
     read S
-
-    isnum='^[0-9]+$'
-    if ! [[ $S =~ $isnum ]]; then
+    if ! [[ $S =~ $is_num ]]; then
         echo error: Not a number. Try again.
         sleep 2
         prompt_file_size
@@ -127,6 +145,7 @@ prompt_file_size() {
 # Ask user if they want the default folders.
 prompt_default_folders() {
     clear
+    echo -e "\n${YELLOW}$heading : FILES AND FOLDERS${NC}\n"
     read -p "Would you like to create a set of default folders in data/groups? (y/n) " yn
     case $yn in
     [yY])
@@ -147,11 +166,12 @@ prompt_default_folders() {
 }
 
 prompt_num_folders() {
-    clear
     existing_scripted_dirs=()
     happy_dirs=()
     sad_dirs=()
 
+    clear
+    echo -e "\n${YELLOW}$heading : FILES AND FOLDERS${NC}\n"
     # Populate existing_scripted_dirs array
     for i in {1..10}; do
         if [ -d "/data/public/folder$i" ]; then
@@ -161,14 +181,15 @@ prompt_num_folders() {
 
     # If folder[1..10] already exists, there's no reason to prompt the user to recreate them.
     if [ "${#existing_scripted_dirs[@]}" -eq 10 ]; then
+        echo -e "${YELLOW}All possible scripted folders already exist!${NC}"
+        sleep 2
         test_existing_files
     fi
 
-    echo -e "\nThis script will create a collection of folders for you (if they do not already exist), labled folder[1..X] (max: 10) in data/public.\n"
+    echo -e "This script will create a collection of folders for you (if they do not already exist), labled folder[1..X] (max: 10) in data/public.\n"
     read -p "How many folders do you want? " nF
 
-    isnum='^[0-9]$|^10$'
-    if ! [[ $nF =~ $isnum ]]; then
+    if ! [[ $nF =~ $is_ranged_num ]]; then
         echo error: "Invalid response. Try again."
         sleep 2
         prompt_num_folders
@@ -176,12 +197,11 @@ prompt_num_folders() {
         # If this was any more complicated, this approach would be too brittle.
         # We're taking in the user input for number of folders they want, appending it to a search string, pushing it to an array to list to the user and doing nothing if we find it.
         for i in $(seq 1 $nF); do
-            folder="/data/public/folder$i"
-            if [ -d "$folder" ]; then
+            if [ -d "/data/public/folder$i" ]; then
                 sad_dirs+=("folder$i")
             else
                 # else push to another array to list to the user, and make the folder.
-                sudo mkdir -p "$folder"
+                sudo mkdir -p "/data/public/folder$i"
                 happy_dirs+=("folder$i")
             fi
         done
@@ -191,8 +211,9 @@ prompt_num_folders() {
 
 # Test to see if there are already files present in target directories. Ask for confirmation before blowing them away.
 test_existing_files() {
-    clear
     all_dirs=($(ls -d /data/public/*/ | xargs -n1 basename))
+    clear
+    echo -e "\n${YELLOW}$heading : FILES AND FOLDERS${NC}\n"
 
     if [ "${#happy_dirs[@]}" -gt 0 ]; then
         echo -e "New directories:\n"
@@ -221,7 +242,7 @@ test_existing_files() {
             create_files
         else
             while $inval_guard; do
-                echo -e "\nFiles are already present in the target directory. If you continue any duplicated files will be overwritten.\n"
+                echo -e "${RED}\nFiles are already present in the target directory. If you continue any duplicated files will be overwritten.${NC}\n"
                 read -p "Are you sure you wish to continue? (y: overwrite duplicates, q: quit, d: delete directory contents and write fresh) " yqd
                 case $yqd in
                 [yY])
@@ -234,7 +255,8 @@ test_existing_files() {
                     ;;
                 [dD])
                     inval_guard=false
-                    rm -rf /data/public/$target/*
+                    sudo rm -rf /data/public/$target/*
+                    sleep 20
                     create_files
                     ;;
                 *)
@@ -267,7 +289,9 @@ create_files() {
 # Simple and dumb delete. Delete all the things. Godspeed.
 delete_all_files() {
     clear
-    read -p "This will remove all files and folder from /data/public and /data/groups. Are you sure you want to do this? (y/n) " yn
+    echo -e "\n${YELLOW}$heading : FILE DELETION${NC}\n"
+    echo -e "${RED}WARNING: This will remove all files and folder from /data/public and /data/groups.${NC}\n"
+    read -p "Are you sure you want to do this? (y/n) " yn
     case $yn in
     [Yy])
         sudo rm -rf /data/public
@@ -290,6 +314,7 @@ delete_all_files() {
 # allows user to use our wizard for network config through nmcli command formatting, or opt to use nmtui
 configure_networking() {
     clear
+    echo -e "\n${YELLOW}$heading : NETWORK CONFIG${NC}"
     echo -e "\n1) I want to use this script to configure network settings \n2) I want to use nmtui to configure network settings \n\nQ) Quit \n"
     read -p "How can I help you today? " input
     case $input in
@@ -311,12 +336,11 @@ configure_networking() {
 }
 
 # prompt for and validate ip address, send to gateway address function if valid. else prompt again.
-# these validators are somewhat flawed. They don't take into account valid IP ranges. But atleast it's something.
-# IP Validation Regex (https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp)
 input_ip_address() {
     clear
+    echo -e "\n${YELLOW}$heading : NETWORK CONFIG${NC}\n"
     read -p "Enter an IP address with CIDR notation (e.g., 192.168.1.0/24): " ip
-    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$ ]]; then
+    if [[ $ip =~ $is_cidr ]]; then
         input_gateway_address "$ip"
     else
         echo "Invalid IP address. Try again"
@@ -328,8 +352,9 @@ input_ip_address() {
 # prompt for and validate gateway address, send to optional dns function if valid, else prompt again.
 input_gateway_address() {
     clear
+    echo -e "\n${YELLOW}$heading : NETWORK CONFIG${NC}\n"
     read -p "Enter your gateway IP address: " gw
-    if [[ $gw =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    if [[ $gw =~ $is_ip ]]; then
         input_dns "$1" "$gw"
     else
         echo "Invalid IP address."
@@ -341,6 +366,7 @@ input_gateway_address() {
 # prompt for optional dns address, if valid, send to confirm, if user opts out, send to confirm with nil dns variable.
 input_dns() {
     clear
+    echo -e "\n${YELLOW}$heading : NETWORK CONFIG${NC}\n"
     local inval_guard=true
     while $inval_guard; do
         read -p "Do you want to override the system default DNS settings? (y/n) " yn
@@ -348,7 +374,7 @@ input_dns() {
         [yY])
             # Not super DRY here. we're repeating logic we already have, but this is easier than redirecting for now.
             read -p "Enter your custom DNS address " dns
-            if [[ $dns =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            if [[ $dns =~ $is_ip ]]; then
                 inval_guard=false
                 confirm_net_config "$1" "$2" "$dns"
             else
@@ -373,17 +399,12 @@ input_dns() {
 # Creates a prompt which allows users to confirm their network config before applying.
 confirm_net_config() {
     clear
+    echo -e "\n${YELLOW}$heading : NETWORK CONFIG${NC}"
     # This is running the IP link command and then using AWK to find the first line that starts with a number, followed by a colon and then a space and then the letter e, which is the interface we want to change.
     iface=$(ip link | awk -F': ' '/^[0-9]+: e/{print $2; exit}')
 
     # print config back to user for confirmation.
-    echo
-    echo "IP : $1"
-    echo "Gateway : $2"
-    echo "DNS : $3"
-    echo "Interface : $iface"
-    echo
-
+    echo -e "\n${GREEN} IP : $1\n Gateway : $2\n DNS : $3\n Interface : $iface${NC}\n "
     read -p "Please confirm you configuration before applying (y: confirm, n: restart config, or q: quit to main menu) " ynq
     case $ynq in
     [yY])
@@ -412,7 +433,8 @@ confirm_net_config() {
 # Ask user if they want to bring the modified interface profile up. Warn that they may be disconnected.
 prompt_interface_up() {
     echo
-    read -p "Do you want to bring up your newly configured connection now? WARNING: this will disconnect you if you are connected via SSH. If you choose not to do this now, you will be taken back to the main menu and the configuration will be appplied on reboot. (y/n). " yn
+    echo -e "\n${RED}WARNING: Bringing up your connection now will disconnect you if you are connected via SSH. If you choose not to do this now, you will be taken back to the main menu and the configuration will be appplied on reboot.${NC}\n"
+    read -p "Do you want to bring up your newly configured connection now? (y/n) " yn
     case $yn in
     [yY])
         sudo nmcli connection up static
@@ -433,9 +455,9 @@ prompt_interface_up() {
 
 # It do what it says it do.
 install_git() {
-    clear
     local inval_guard=true
-
+    clear
+    echo -e "\n${YELLOW}$heading : GIT CONFIG${NC}\n"
     # Check if git installed. Silence output - if it aint. install it.
     if ! command -v git &>/dev/null; then
         echo "Git is not installed. Installing Git..."
@@ -466,6 +488,7 @@ install_git() {
 # Create SSH config  for user. Lovely lil nested conditionals.
 create_ssh_config() {
     clear
+    echo -e "\n${YELLOW}$heading : GIT CONFIG${NC}\n"
     read -p "Please name your ssh key (no spaces allowed) " name
 
     # Check if it has spaces in it. We don't like spaces.
@@ -482,11 +505,6 @@ create_ssh_config() {
         else
             echo "Creating SSH key..."
             ssh-keygen -t rsa -N "" -f ~/.ssh/$name
-            # If they aint got a config file already, make it.
-            if [ ! -f ~/.ssh/config ]; then
-                echo "No existing SSH config file. Creating a new config file..."
-                touch ~/.ssh/config
-            fi
             # If they already have a config entry for github. Don't make it complicated. Tell them to look at the file.
             if grep -q "Host github.com" ~/.ssh/config; then
                 echo "You already have a config file entry for github. Please review your config file."
@@ -507,8 +525,10 @@ create_ssh_config() {
 # Multi choice menu for different storage configuration options
 prompt_config_storage() {
     clear
-    echo -e "STORAGE CONFIGURATION :\n"
+    echo -e "\n${YELLOW}$heading : STORAGE CONFIG${NC}\n"
+    printf "${GREEN}"
     lsblk
+    printf "${NC}"
     echo -e "\nOPTIONS :\n\n1) I want to use this script to automatically configure /dev/sdc\n2) I want to use fdisk to manually configure /dev/sdc\n3) unmount /dev/sdc1\n4) unmount and zero /dev/sdc\n\nQ) Quit \n"
     read -p "How can I help you today? " input
     case $input in
@@ -520,6 +540,8 @@ prompt_config_storage() {
         ;;
     3)
         sudo umount /dev/sdc1
+        sleep 2
+        call_menu
         ;;
     4)
         confirm_destroy
@@ -539,9 +561,10 @@ prompt_config_storage() {
 # If /dev/sdc exists, advance prompt. If not bounce user out and tell them to figure themselves out.
 check_storage() {
     clear
+    echo -e "\n${YELLOW}$heading : STORAGE CONFIG${NC}\n"
     if lsblk -f /dev/sdc >/dev/null 2>&1; then
         if [ -d /data ]; then
-            echo -e "WARNING: You already have a folder located at /data. This script will mount a drive at that location. If there is data present in this folder it will become hidden until the drive is unmounted.\n"
+            echo -e "${RED}WARNING: You already have a folder located at /data. This script will mount a drive at that location. If there is data present in this folder it will become hidden until the drive is unmounted.${NC}\n"
             read -p "Are you sure you wish to continue? (y: continue, n: exit) " input
             case $input in
             [yY])
@@ -569,6 +592,7 @@ check_storage() {
 # Automated flow for disk configuration in spec with NOS-125 requirements. Create /etc/fstab entry for automatic mounting.
 auto_config_storage() {
     clear
+    echo -e "\n${YELLOW}$heading : STORAGE CONFIG${NC}\n"
     # if /data doesn't exist make it.
     if [ ! -d /data ]; then
         sudo mkdir /data
@@ -576,7 +600,7 @@ auto_config_storage() {
 
     # Guard clause. If we already have our partition set up. do nothing.
     if lsblk -f /dev/sdc1 >/dev/null 2>&1; then
-        echo "/dev/sdc1 has already been configured or already contains a file system. Automatic configuration is risky."
+        echo -e "${RED}/dev/sdc1 has already been configured or already contains a file system. Automatic configuration is risky.${NC}"
         echo "The script will return you to the storage configuration menu..."
         sleep 3
         prompt_config_storage
@@ -602,7 +626,8 @@ auto_config_storage() {
 # Confirmation dialog and commands for zeroing disks.
 confirm_destroy() {
     clear
-    echo "Woah! Let's not be too hasty... This is going to delete everything on /dev/sdc !!!"
+    echo -e "\n${YELLOW}$heading : STORAGE CONFIG${NC}\n"
+    echo -e "${RED}Woah! Let's not be too hasty... This is going to delete everything on /dev/sdc!!!${NC}\n"
     read -p "Are you sure about this? (y/n) " yn
     case $yn in
     [yY])
